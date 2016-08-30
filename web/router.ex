8cf788c62bff26ece721d01b0a0f1289504a7eb1
qgadrian <qgadrian@gmail.com>
2016-08-30 20:28:29 +0200

    tmp

--- web/router.ex
+++ web/router.ex
@@ -1,5 +1,6 @@
 defmodule TestApp.Router do
   use TestApp.Web, :router
+  use Coherence.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -7,16 +8,29 @@ defmodule TestApp.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session
   end
 
   pipeline :api do
     plug :accepts, ["json"]
-    plug Guardian.Plug.VerifyHeader # Looks for token in authorization header
-    plug Guardian.Plug.LoadResource # Makes the current resource available Guardian.Plug.current_resource(conn)
+#    plug Guardian.Plug.VerifyHeader # Looks for token in authorization header
+#    plug Guardian.Plug.LoadResource # Makes the current resource available Guardian.Plug.current_resource(conn)
+    plug Coherence.Authentication.Session
+  end
+
+  scope "/" do
+    pipe_through :browser
+    coherence_routes
+  end
+
+  scope "/" do
+    pipe_through :protected
+    coherence_routes :protected
   end
 
   scope "/", TestApp do
     pipe_through :browser # Use the default browser stack
+    coherence_routes
 
     get "/", PageController, :index
   end
